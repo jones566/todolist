6f0569bfaa6fdbcc7f5b4ec2088ece969c7fc069
Jones Tweneboah <tweneboahjones07@gmail.com>
2022-03-03 13:24:21 +0000

    todolist

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+import  {createApp} from 'vue';
+import App from './components/App.vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
+
